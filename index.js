039101b47184f1f1155a3d91b9a2ca556dd50375
Omtech <tokeshwars@webmobtech.com>
2023-05-11 15:36:39 +0530

    cf

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import app from "./app.js";
 import { sequelize } from "./database/database.js";
-import "./models/Association.js"; // import the associations file
 
 async function main() {
   await sequelize.sync({ force: false });
--- models/Association.js
+++
@@ -1,7 +0,0 @@
-import { User } from "./User.js";
-import Cart from "./Cart.js";
-import Product from "./Product.js";
-
-Cart.belongsTo(User, { foreignKey: "userId" });
-Cart.belongsToMany(Product, { through: "cart_product", foreignKey: "cartId" });
-Product.belongsToMany(Cart, { through: "cart_product", foreignKey: "productId" });
